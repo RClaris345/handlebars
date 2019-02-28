eca88bb150a1fd3704b1b4876edf8a9fcead85c2
RvClariss <clarissa.rvs345@gmail.com>
2019-02-28 13:40:44 -0700

    finsihed

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Mac N Cheese');
+INSERT INTO burgers (name) VALUES ('Chili');
+INSERT INTO burgers (name) VALUES ('PB&J');
+INSERT INTO burgers (name) VALUES ('Fried');
+INSERT INTO burgers (name) VALUES ('Blue Cheese');
+INSERT INTO burgers (name) VALUES ('Koffee');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,47 @@
+
+
+$(document).ready(function() {
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+    console.log("boogers")
+
+    var newBurger = $("#ca")
+      .val()
+      .trim();
+    $.ajax({
+      method: "POST",
+      url: "/api/burgers",
+      data: {
+        newBurger
+      }
+    }).then(function(data) {
+      console.log("boogers")
+      location.reload();
+    });
+  });
+
+  $(".change-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    var eatenBurgers = {
+      eaten: true
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: eatenBurgers
+    }).then(function() {
+      // Reload the page to get the updated list
+      location.reload();
+    });
+
+    //     console.log(newBurger);
+    //     $.ajax({
+    //       method: "PUT",
+    //       url: "/api/burgers/" + newBurger,
+    //     }).then(function(data){
+    //   location.reload();
+    // })
+  });
+});
